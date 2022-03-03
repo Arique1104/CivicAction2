b015bb774f26fa99df5464e7a7d11507ae196506
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2022-03-03 11:10:21 -0700

    Leadboard Joins tables for User and Activity

---
+++ db/migrate/20220303180929_create_leaderboard_users.rb
@@ -0,0 +1,10 @@
+class CreateLeaderboardUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :leaderboard_users do |t|
+      t.references :leaderboard, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
